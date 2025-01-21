ee110b9859eb022765e167df808ea2f9b5c84b3a
cherpinskif <107156674+cherpinskif@users.noreply.github.com>
2025-01-21 00:29:40 -0300

    correção caminho pasta

--- 1_home.py
+++ 1_home.py
@@ -9,7 +9,7 @@ st.set_page_config(layout='wide')
 if 'data' not in st.session_state:
     year = ['17','18','19','20','21','22','23']
     select_year = st.sidebar.selectbox("Selecione o ano que você deseja?",year)
-    df_times = pd.read_csv(r'https://github.com/cherpinskif/FIFAStreamlit/blob/main/datasets/CLEAN_FIFA17_official_data.csv',index_col=0)
+    df_times = pd.read_csv(r'datasets/CLEAN_FIFA17_official_data.csv',index_col=0)
     df_times = df_times[df_times["Contract Valid Until"] >= datetime.today().year]
     df_times = df_times[df_times["Value(£)"] > 0]
     df_times = df_times.sort_values(by="Overall", ascending=False)
