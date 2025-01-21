07f4a753e262169b330495e9cd735c8eacce64c3
cherpinskif <107156674+cherpinskif@users.noreply.github.com>
2025-01-21 00:31:33 -0300

    correção caminho pasta

--- pages/3_teams.py
+++ pages/3_teams.py
@@ -8,16 +8,9 @@ st.set_page_config(layout='wide')
 
 df_data = st.session_state['data']
 
-arquivos = os.getcwd()+r"\datasets"
-lista = os.listdir(arquivos)
-lista = [listas for listas in lista]
-year = []
-for x in lista:
-    x = re.findall("\d+",x)
-    year.extend(x)
-    
+year = ['17','18','19','20','21','22','23']
 select_year = st.sidebar.selectbox("Selecione o ano que você deseja?",year)
-df_times = pd.read_csv(arquivos+'\\CLEAN_FIFA'+str(select_year)+'_official_data.csv',index_col=0)
+df_times = pd.read_csv(r'datasets/CLEAN_FIFA'+year+'_official_data.csv',index_col=0)
 df_times = df_times[df_times["Contract Valid Until"] >= datetime.today().year]
 df_times = df_times[df_times["Value(£)"] > 0]
 df_times = df_times.sort_values(by="Overall", ascending=False)
